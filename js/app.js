a99530213a70d3d6c1e8f52eb9088838bfe3aae9
Ben Pinali <ben@benpinali.com>
2016-03-01 21:39:41 -0600

    Removed HTML placeholders, added JS for showing results from product array, new CSS card styles

--- js/app.js
+++ js/app.js
@@ -7,11 +7,13 @@ $(document).ready(function () {
             url: url,
             data: {
                 format: 'json',
-                apiKey: 't5reggzup769kevta2bdabkx'
+                apiKey: 't5reggzup769kevta2bdabkx',
+                page: 1,
+                pageSize: 36
             },
             cache: true, // necessary because our API rejects queries with unrecognized query parameters, such as the underscore injected when this isn't included
-            active: true,
             preowned: false,
+            active: true,
             dataType: 'jsonp'
         }).done(
             function (result) {
@@ -30,15 +32,32 @@ $(document).ready(function () {
 
                 }
                 var output = "";
+
                 if (!result.error && result.products) {
-                    output = "Products:\n";
                     result.products.forEach(function (product) {
-                        output += "\t* " + product.name + "\n";
+                        output += '<li>';
+                        output += '<h3>' + product.name + '</h3>';
+                        output += '<img src="' + product.image + '">';
+                        if (product.customerReviewCount != null) {
+                            output += '<p class="review-num">' + product.customerReviewCount + ' Reviews</p>';
+                        }
+                        if (product.customerReviewAverage != null) {
+                            output += '<p class="star-avg">' + product.customerReviewAverage + ' Stars</p>';
+                        }
+
+                        if ((product.salePrice < product.regularPrice) && (product.salePrice != null)) {
+                            output += '<p class="reg-price strikethrough">$' + product.regularPrice + '</p>';
+                            output += '<p class="sale-price highlight">Sale: $' + product.salePrice + '</p>';
+                        } else {
+                            output += '<p class="reg-price strong">$' + product.regularPrice + '</p>';
+                        }
+                        output += '<a href="' + product.addToCartUrl + '" class="add-to-cart">Add to Cart</a>';
+                        output += '</li>';
                     });
                 } else {
                     output = "Unable to access products (see browser console for more information)";
                 }
-                $('.results').append(output);
+                $('.results ul').append(output);
             }
         ).fail(function (jqXHR, error, errorThrown) {
             console.log(jqXHR);
