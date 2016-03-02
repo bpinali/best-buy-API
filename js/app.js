575a513c641ae9561accd4bf1b53395c36220db8
Ben Pinali <ben@benpinali.com>
2016-03-01 18:52:57 -0600

    Added normalize, basic CSS styles and HTML structure

--- js/app.js
+++ js/app.js
@@ -10,10 +10,25 @@ $(document).ready(function () {
                 apiKey: 't5reggzup769kevta2bdabkx'
             },
             cache: true, // necessary because our API rejects queries with unrecognized query parameters, such as the underscore injected when this isn't included
+            active: true,
+            preowned: false,
             dataType: 'jsonp'
-        }).always(
+        }).done(
             function (result) {
-                console.log(result.products[0].addToCartUrl);
+                if (result.products.length == 0) {
+                    alert('No Results!');
+                } else {
+                    console.log(result);
+                    console.log(result.products[0].image);
+                    console.log(result.products[0].name);
+                    console.log(result.products[0].regularPrice);
+                    console.log(result.products[0].salePrice);
+                    console.log(result.products[0].customerReviewCount);
+                    console.log(result.products[0].customerReviewAverage);
+                    console.log(result.products[0].customerTopRated);
+                    console.log(result.products[0].addToCartUrl);
+
+                }
                 var output = "";
                 if (!result.error && result.products) {
                     output = "Products:\n";
@@ -25,7 +40,11 @@ $(document).ready(function () {
                 }
                 $('.results').append(output);
             }
-        );
+        ).fail(function (jqXHR, error, errorThrown) {
+            console.log(jqXHR);
+            console.log(error);
+            console.log(errorThrown);
+        });
     }
     $('#search-button').on('click', function () {
         getResults($("#search-box").val());
