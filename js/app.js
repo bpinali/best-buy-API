290d47b88f9a11376ea37b5b2cc936ae47afe24d
Ben Pinali <ben@benpinali.com>
2016-03-14 21:17:51 -0500

    Removing console logging.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     function getResults(query) {
         var url = 'https://api.bestbuy.com/v1/products((name=' + query + '*)&type!=BlackTie&customerTopRated=true)?sort=salesRankShortTerm.asc';
-        console.log(query);
         $.ajax({
             method: 'GET',
             url: url,
@@ -20,62 +19,50 @@ $(document).ready(function () {
                 if (result.products.length == 0) {
                     alert('No Results Found!');
                 } else {
-                    console.log(url);
-                    console.log(result);
-                    console.log(result.products[0].image);
-                    console.log(result.products[0].name);
-                    console.log(result.products[0].regularPrice);
-                    console.log(result.products[0].salePrice);
-                    console.log(result.products[0].customerReviewCount);
-                    console.log(result.products[0].customerReviewAverage);
-                    console.log(result.products[0].customerTopRated);
-                    console.log(result.products[0].addToCartUrl);
+                    var output = "";
 
-                }
-                var output = "";
+                    if (!result.error && result.products) {
+                        result.products.forEach(function (product) {
+                            output += '<li>';
+                            output += '<div class="product-container">';
+                            output += '<div class="title-wrapper"><h3 class="clamp-this">' + product.name + '</h3></div>';
+                            output += '<img src="' + product.image + '">';
+                            output += '<div class = "product-details">';
+                            if (product.customerReviewCount != null) {
+                                output += '<p class="review-num">' + product.customerReviewCount + ' Reviews</p>';
+                            }
+                            if (product.customerReviewAverage != null) {
+                                output += '<p class="star-avg">' + product.customerReviewAverage + ' Stars</p>';
+                            }
 
-                if (!result.error && result.products) {
-                    result.products.forEach(function (product) {
-                        output += '<li>';
-                        output += '<div class="product-container">';
-                        output += '<div class="title-wrapper"><h3 class="clamp-this">' + product.name + '</h3></div>';
-                        output += '<img src="' + product.image + '">';
-                        output += '<div class = "product-details">';
-                        if (product.customerReviewCount != null) {
-                            output += '<p class="review-num">' + product.customerReviewCount + ' Reviews</p>';
-                        }
-                        if (product.customerReviewAverage != null) {
-                            output += '<p class="star-avg">' + product.customerReviewAverage + ' Stars</p>';
-                        }
-
-                        if ((product.salePrice < product.regularPrice) && (product.salePrice != null)) {
-                            output += '<p class="reg-price strikethrough">$' + product.regularPrice + '</p>';
-                            output += '<p class="sale-price highlight">Sale: $' + product.salePrice + '</p>';
-                            var isSale = true;
-                        } else {
-                            output += '<p class="reg-price strong no-sale">$' + product.regularPrice + '</p>';
-                            var isSale = false;
-                        }
-                        output += '</div>';
-                        if (isSale == false) {
-                            output += '<a href="' + product.addToCartUrl + '" class="add-to-cart">Add to Cart</a>';
-                        } else {
-                            output += '<a href="' + product.addToCartUrl + '" class="add-to-cart sale-button">Add to Cart</a>';
-                        }
-                        output += '</div>';
-                        output += '</li>';
+                            if ((product.salePrice < product.regularPrice) && (product.salePrice != null)) {
+                                output += '<p class="reg-price strikethrough">$' + product.regularPrice + '</p>';
+                                output += '<p class="sale-price highlight">Sale: $' + product.salePrice + '</p>';
+                                var isSale = true;
+                            } else {
+                                output += '<p class="reg-price strong no-sale">$' + product.regularPrice + '</p>';
+                                var isSale = false;
+                            }
+                            output += '</div>';
+                            if (isSale == false) {
+                                output += '<a href="' + product.addToCartUrl + '" class="add-to-cart">Add to Cart</a>';
+                            } else {
+                                output += '<a href="' + product.addToCartUrl + '" class="add-to-cart sale-button">Add to Cart</a>';
+                            }
+                            output += '</div>';
+                            output += '</li>';
+                        });
+                    } else {
+                        output = "Unable to access products (see browser console for more information)";
+                    }
+                    $('.results ul').html(output);
+                    $('.clamp-this').each(function (index, element) {
+                        $clamp(element, {
+                            clamp: 3
+                        });
                     });
-                } else {
-                    output = "Unable to access products (see browser console for more information)";
+                    $(".loader").fadeOut("slow");
                 }
-                $('.results ul').html(output);
-                $('.clamp-this').each(function (index, element) {
-                    console.log(element);
-                    $clamp(element, {
-                        clamp: 3
-                    });
-                });
-                $(".loader").fadeOut("slow");
             }
         ).fail(function (jqXHR, error, errorThrown) {
             console.log(jqXHR);
